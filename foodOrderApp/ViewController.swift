2d95340676c166020d90395a4d2c51cb7e087ea5
keremec <k.safa.dirican@outlook.com>
2022-09-06 12:26:02 +0300

    Initial Commit

---
+++ foodOrderApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  foodOrderApp
+//
+//  Created by Kerem Safa Dirican on 6.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
